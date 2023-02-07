31d4ade3ed8cce905cb3076e7e9efd21709a95ec
Filip Filev <124343742+FilipFilev@users.noreply.github.com>
2023-02-07 15:23:38 +0200

    if else , if else , if else

--- src/ConditionalStatementsAdvanced070223/Lab/P05_SmallShop.java
+++ src/ConditionalStatementsAdvanced070223/Lab/P05_SmallShop.java
@@ -11,46 +11,45 @@ public class P05_SmallShop {
         double quantity = Double.parseDouble(scanner.nextLine());
 
         double price = 0;
-        switch (town) {
-            case "Sofia" -> {
-                if (product.equals("coffee")) {
-                    price = 0.50;
-                } else if (product.equals("water")) {
-                    price = 0.80;
-                } else if (product.equals("beer")) {
-                    price = 1.20;
-                } else if (product.equals("sweets")) {
-                    price = 1.45;
-                } else if (product.equals("peanuts")) {
-                    price = 1.60;
-                }
+        if (town.equals("Sofia")) {
+            if (product.equals("coffee")) {
+                price = 0.50;
+            } else if (product.equals("water")) {
+                price = 0.80;
+            } else if (product.equals("beer")) {
+                price = 1.20;
+            } else if (product.equals("sweets")) {
+                price = 1.45;
+            } else if (product.equals("peanuts")) {
+                price = 1.60;
             }
-            case "Plovdiv" -> {
-                if (product.equals("coffee")) {
-                    price = 0.40;
-                } else if (product.equals("water")) {
-                    price = 0.70;
-                } else if (product.equals("beer")) {
-                    price = 1.15;
-                } else if (product.equals("sweets")) {
-                    price = 1.30;
-                } else if (product.equals("peanuts")) {
-                    price = 1.50;
-                }
+
+        } else if (town.equals("Plovdiv")) {
+            if (product.equals("coffee")) {
+                price = 0.40;
+            } else if (product.equals("water")) {
+                price = 0.70;
+            } else if (product.equals("beer")) {
+                price = 1.15;
+            } else if (product.equals("sweets")) {
+                price = 1.30;
+            } else if (product.equals("peanuts")) {
+                price = 1.50;
             }
-            case "Varna" -> {
-                if (product.equals("coffee")) {
-                    price = 0.45;
-                } else if (product.equals("water")) {
-                    price = 0.70;
-                } else if (product.equals("beer")) {
-                    price = 1.10;
-                } else if (product.equals("sweets")) {
-                    price = 1.35;
-                } else if (product.equals("peanuts")) {
-                    price = 1.55;
-                }
+
+        } else if (town.equals("Varna")) {
+            if (product.equals("coffee")) {
+                price = 0.45;
+            } else if (product.equals("water")) {
+                price = 0.70;
+            } else if (product.equals("beer")) {
+                price = 1.10;
+            } else if (product.equals("sweets")) {
+                price = 1.35;
+            } else if (product.equals("peanuts")) {
+                price = 1.55;
             }
+
         }
         double result = price * quantity;
         System.out.println(result);
