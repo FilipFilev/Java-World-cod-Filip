e759bf4da810bbdb7a2a6e906997818e20584f11
Filip Filev <124343742+FilipFilev@users.noreply.github.com>
2023-02-07 17:28:53 +0200

    if (!invalid)

--- src/ConditionalStatementsAdvanced070223/Lab/P10_InvalidNumber.java
+++ src/ConditionalStatementsAdvanced070223/Lab/P10_InvalidNumber.java
@@ -6,15 +6,11 @@ public class P10_InvalidNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
         int num = Integer.parseInt(scanner.nextLine());
-
-
         boolean valid = num >= 100 && num <= 200 || num == 0;
 
         if (!valid) {
             System.out.println("invalid");
         }
-
     }
 }
