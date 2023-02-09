e4dc17c378a7ebf62b38f817252f8009d7e3020b
filip_filev <aquamarine777@abv.bg>
2023-02-09 11:44:44 +0200

    Scanner

---
+++ src/ConditionalStatementsAdvanced070223/Exercises/P09_SkiVacation.java
@@ -0,0 +1,10 @@
+package ConditionalStatementsAdvanced070223.Exercises;
+
+import java.util.Scanner;
+
+public class P09_SkiVacation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
