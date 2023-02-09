4b3e94c153811e31b1ec6988acdb388291dccacc
filip_filev <aquamarine777@abv.bg>
2023-02-09 16:10:26 +0200

    Scanner

--- src/ConditionalStatementsAdvanced070223/Exercises/P09_SkiVacation.java
+++ src/ConditionalStatementsAdvanced070223/Exercises/P09_SkiVacation.java
@@ -1,10 +1,29 @@
 package ConditionalStatementsAdvanced070223.Exercises;
 
+import java.util.InvalidPropertiesFormatException;
 import java.util.Scanner;
 
 public class P09_SkiVacation {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        //1. Прочитаме входни данни
+        //2. пресмятаме точно колко нощувки ще има
+        //3. switch в зависимост от типа стая - пресмятаме цената
+         //3а Проверка дали ще има отстъпкла
+        //3б пресмятаме отстъпката
+        //4 Проверкана оценката
+        //5 крайнат цена се смята спрямо оценката
+        //6 принтираме крайната сума
+
+
+        int days = Integer.parseInt(scanner.nextLine());
+        String roomType = scanner.nextLine();
+        String assessment = scanner.nextLine();
+
+
+
+
 
     }
 }
