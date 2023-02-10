2db679654a69e1187477c6c7822f55c9fbc14813
filip_filev <aquamarine777@abv.bg>
2023-02-10 14:19:26 +0200

    for

---
+++ src/ForLoop10022023/Lab/P01_Numbers1To100.java
@@ -0,0 +1,17 @@
+package ForLoop10022023.Lab;
+
+
+
+public class P01_Numbers1To100 {
+    public static void main(String[] args) {
+
+
+//Напишете програма, която отпечатва числата от 1 до 100, всяко на нов ред.
+        for (int i = 1; i <= 100; i++ ) {
+            System.out.println(i);
+        }
+
+
+
+    }
+}
