c0af5b42a489ad495ffeca6e8bbd9adce4846670
filip_filev <aquamarine777@abv.bg>
2023-02-10 17:43:05 +0200

    int sum = 0;         //hello         for (int i = 0; i < text.length(); i++) {             char currentSymbol = text.charAt(i);
                switch (currentSymbol) {
                    case 'a':
                        sum = sum + 1;
                        break;
                    case 'e':
                        sum = sum + 2;

--- src/ForLoop10022023/Lab/P06_VowelsSum.java
+++ src/ForLoop10022023/Lab/P06_VowelsSum.java
@@ -12,12 +12,34 @@ public class P06_VowelsSum {
 
 
         int sum = 0;
+        //hello
         for (int i = 0; i < text.length(); i++) {
             char currentSymbol = text.charAt(i);
 
-            System.out.println(currentSymbol);
+
+            switch (currentSymbol) {
+                case 'a':
+                    sum = sum + 1;
+                    break;
+                case 'e':
+                    sum = sum + 2;
+                    break;
+                case 'i':
+                    sum = sum + 3;
+                    break;
+                case 'o':
+                    sum = sum + 4;
+                    break;
+                case 'u':
+                    sum = sum + 5;
+                    break;
+
+            }
+
+
         }
 
 
+        System.out.println(sum);
     }
 }
