804640184a9492ac4cc2f13d9238b9436798dde2
filip_filev <aquamarine777@abv.bg>
2023-02-10 14:16:00 +0200

    for

--- src/ForLoop10022023/Lab/Demo.java
+++ src/ForLoop10022023/Lab/Demo.java
@@ -1,13 +1,13 @@
 package ForLoop10022023.Lab;
 
-import java.util.Scanner;
+
 
 public class Demo {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
 
-        for (int i = 1; i <= 12; i++ ) {
+//Напишете програма, която отпечатва числата от 1 до 100, всяко на нов ред.
+        for (int i = 1; i <= 100; i++ ) {
             System.out.println(i);
         }
 
