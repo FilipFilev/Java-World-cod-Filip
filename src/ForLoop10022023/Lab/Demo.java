1db0ecee47be065fc49d87a468f9a13e39d61477
filip_filev <aquamarine777@abv.bg>
2023-02-10 13:19:43 +0200

    demo

---
+++ src/ForLoop10022023/Lab/Demo.java
@@ -0,0 +1,12 @@
+package ForLoop10022023.Lab;
+
+import java.util.Scanner;
+
+public class Demo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+    }
+}
