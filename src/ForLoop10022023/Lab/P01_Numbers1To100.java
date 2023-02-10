f252726e9c8634458752a2592a87e548580a409a
filip_filev <aquamarine777@abv.bg>
2023-02-10 15:14:50 +0200

    for (int i = n; i >= 1 ; i--) {             System.out.println(i);

--- src/ForLoop10022023/Lab/P01_Numbers1To100.java
+++ src/ForLoop10022023/Lab/P01_Numbers1To100.java
@@ -1,16 +1,14 @@
 package ForLoop10022023.Lab;
 
 
-
 public class P01_Numbers1To100 {
     public static void main(String[] args) {
 
 
 //Напишете програма, която отпечатва числата от 1 до 100, всяко на нов ред.
-        for (int i = 1; i <= 100; i++ ) {
-            System.out.println(i);
+        for (int i = 1; i <= 100; i++) {
+            System.out.printf("%d ", i);
         }
-
 
 
     }
--- src/ForLoop10022023/Lab/P02_NumbersNto1.java
+++ src/ForLoop10022023/Lab/P02_NumbersNto1.java
@@ -1,4 +1,23 @@
 package ForLoop10022023.Lab;
 
+import java.util.Scanner;
+
 public class P02_NumbersNto1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        int n = Integer.parseInt(scanner.nextLine());
+        //5
+
+        for (int i = n; i >= 1 ; i--) {
+            System.out.println(i);
+        }
+
+
+
+
+
+
+    }
 }
