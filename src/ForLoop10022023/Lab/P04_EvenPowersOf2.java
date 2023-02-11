daa306a970854c30326d69fe1b8a6b33e03e6301
Filip Filev <124343742+FilipFilev@users.noreply.github.com>
2023-02-11 15:38:39 +0200

    //n 7 -> 0 2 4 6         int num = 1;         for (int i = 0;  i <= n; i += 2) {             System.out.println(num);             num = num * 2 * 2;

--- src/ForLoop10022023/Lab/P04_EvenPowersOf2.java
+++ src/ForLoop10022023/Lab/P04_EvenPowersOf2.java
@@ -6,8 +6,21 @@ public class P04_EvenPowersOf2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        
+        int n = Integer.parseInt(scanner.nextLine());
 
+        //n 7 -> 0 2 4 6
+        int num = 1;
+        for (int i = 0;  i <= n; i += 2) {
+            System.out.println(num);
+            num = num * 2 * 2;
+
+
+
+
+
+        }
+
+        
 
 
 
