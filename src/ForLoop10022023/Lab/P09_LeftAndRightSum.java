c57f5d7ebd95cf0a9aab8321148e95bb41cfe52a
Filip Filev <124343742+FilipFilev@users.noreply.github.com>
2023-02-11 14:20:01 +0200

    int currnetNum = Integer.parseInt(scanner.nextLine());
                if (i <= n) {
                    leftSum = leftSum + currnetNum;
    
                } else {
                    rightSum = rightSum + currnetNum;

---
+++ src/ForLoop10022023/Lab/P09_LeftAndRightSum.java
@@ -0,0 +1,49 @@
+package ForLoop10022023.Lab;
+
+import java.util.Scanner;
+import java.util.function.BinaryOperator;
+
+public class P09_LeftAndRightSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        int leftSum = 0;
+        int rightSum = 0;
+        for (int i = 1; i <= 2 * n; i++) {
+            int currnetNum = Integer.parseInt(scanner.nextLine());
+
+            if (i <= n) {
+                leftSum = leftSum + currnetNum;
+
+            } else {
+                rightSum = rightSum + currnetNum;
+            }
+
+//            leftSum = leftSum + currnetNum;
+//
+//        }
+//
+//        int rightSum = 0;
+//        for (int i = 1; i <= n; i++) {
+//            int currentNum = Integer.parseInt(scanner.nextLine());
+//            rightSum = rightSum + currentNum;
+//
+//        }
+//
+            if (leftSum == rightSum) {
+                System.out.printf("Yes, sum = %d%n", leftSum);
+            } else {
+                int diff = Math.abs(leftSum - rightSum);
+//
+//
+                System.out.printf("No, diff = %d%n", diff);
+
+
+            }
+
+        }
+    }
+}
