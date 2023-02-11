40f7458fd97b73b4c238b65ad1290c5216363081
Filip Filev <124343742+FilipFilev@users.noreply.github.com>
2023-02-11 21:15:32 +0200

    Filip

--- src/ForLoop10022023/Exercises/Demo.java
+++ src/ForLoop10022023/Exercises/Demo.java
@@ -6,5 +6,15 @@ public class Demo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String name = "Filip";
+
+
+        for (int i = 0; i < name.length(); i++) {
+
+            System.out.println(name.charAt(i));
+            
+        }
+        
+
+        }
     }
-}
