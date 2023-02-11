824bd4872cadf032cada3f5180c5126f41ec0723
Filip Filev <124343742+FilipFilev@users.noreply.github.com>
2023-02-11 16:06:29 +0200

    demo

---
+++ src/ForLoop10022023/Exercises/Demo.java
@@ -0,0 +1,10 @@
+package ForLoop10022023.Exercises;
+
+import java.util.Scanner;
+
+public class Demo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
