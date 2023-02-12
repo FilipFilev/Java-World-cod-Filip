6267cdf579776cd5cf4d7a4edd904c6a55acaf56
Filip Filev <124343742+FilipFilev@users.noreply.github.com>
2023-02-12 09:41:05 +0200

    edit

--- src/ForLoop10022023/Lab/P05_CharacterSequence.java
+++ src/ForLoop10022023/Lab/P05_CharacterSequence.java
@@ -11,11 +11,21 @@ public class P05_CharacterSequence {
 
         for (int i = 0; i < text.length(); i++) {
             System.out.println(text.charAt(i));
-
         }
-
-
-
-
     }
 }
+//package forLoops;
+//
+//import java.util.Scanner;
+//
+//public class P05CharacterSequence {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        String text = scanner.nextLine();
+//
+//        for (int i = 0; i < text.length(); i++) {
+//            System.out.println(text.charAt(i));
+//        }
+//    }
+//}
