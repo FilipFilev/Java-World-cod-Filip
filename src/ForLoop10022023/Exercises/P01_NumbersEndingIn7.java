2a34044dfe270de0d1014948a730858d204c432e
Filip Filev <124343742+FilipFilev@users.noreply.github.com>
2023-02-12 08:46:22 +0200

    NumbersEndingIn7

---
+++ src/ForLoop10022023/Exercises/P01_NumbersEndingIn7.java
@@ -0,0 +1,20 @@
+package ForLoop10022023.Exercises;
+
+import java.util.Scanner;
+
+public class P01_NumbersEndingIn7 {
+    public static void main(String[] args) {
+
+        for (int i = 7; i <= 997 ; i++) {
+
+
+            if (i % 10 ==7) {
+                System.out.println(i);
+            }
+
+
+        }
+
+    }
+
+}
