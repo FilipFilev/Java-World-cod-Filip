d4041c58a6365502e04716657e48c0a5e7e13cf4
filip_filev <aquamarine777@abv.bg>
2023-02-13 12:00:51 +0200

    Demo

---
+++ src/ForLoop10022023/Exercises/Demo.java
@@ -0,0 +1,13 @@
+package ForLoop10022023.Exercises;
+
+import java.util.Scanner;
+
+public class Demo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        int num = Integer.parseInt(scanner.nextLine());
+        System.out.println(num);
+    }
+}
