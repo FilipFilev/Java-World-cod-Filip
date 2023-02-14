902526468f2d90b709dbf095ae04a865c0767064
filip_filev <aquamarine777@abv.bg>
2023-02-14 17:42:56 +0200

    add +3

--- src/ForLoop10022023/Exercises/P02_HalfSumElement.java
+++ src/ForLoop10022023/Exercises/P02_HalfSumElement.java
@@ -28,7 +28,7 @@ public class P02_HalfSumElement {
 
         } else {
             int diff = Math.abs(sumWithoutMaxNumber - maxNum);
-            System.out.println("Noо0o0");
+            System.out.println("Noо0o0o");
             System.out.printf("Diff = %d%n", diff);
         }
     }
