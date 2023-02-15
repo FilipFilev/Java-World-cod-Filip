9a8ed90f31bc39408c731711acbf3914caaf0e52
filip_filev <aquamarine777@abv.bg>
2023-02-15 12:06:00 +0200

    while (!text.equals("Stop")) {
                System.out.println(text);
                text = scanner.nextLine();

---
+++ src/WhileLoop150223/Lab/P01ReadText.java
@@ -0,0 +1,20 @@
+package WhileLoop150223.Lab;
+
+import java.util.Scanner;
+
+public class P01ReadText {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String text = scanner.nextLine();
+
+        while (!text.equals("Stop")) {
+
+            System.out.println(text);
+            text = scanner.nextLine();
+
+        }
+
+    }
+
+}
