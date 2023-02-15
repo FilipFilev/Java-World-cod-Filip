bcb0bc2a90f23ac8104a65f41989e38778183bd5
filip_filev <aquamarine777@abv.bg>
2023-02-15 14:45:38 +0200

    while (number <= n) {
                System.out.println(number);
                number = number * 2 +1;
    
            }

---
+++ src/WhileLoop150223/Lab/P04Sequence2kPlus1.java
@@ -0,0 +1,21 @@
+package WhileLoop150223.Lab;
+
+import java.util.Scanner;
+
+public class P04Sequence2kPlus1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        int number = 1;
+
+        while (number <= n) {
+
+            System.out.println(number);
+            number = number * 2 +1;
+
+        }
+
+    }
+}
