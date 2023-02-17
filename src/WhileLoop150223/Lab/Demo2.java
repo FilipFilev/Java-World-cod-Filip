41db8b44aede90f8c04329f42c3cdd75a26961df
Filip Filev <124343742+FilipFilev@users.noreply.github.com>
2023-02-17 22:58:36 +0200

    gold

--- src/WhileLoop150223/Lab/Demo2.java
+++ src/WhileLoop150223/Lab/Demo2.java
@@ -1,15 +1,14 @@
 package WhileLoop150223.Lab;
 
 
-import java.util.Scanner;
+
 
 public class Demo2 {
     public static void main(String[] args) {
-Scanner scanner = new Scanner(System.in);
 
-        String gold = scanner.nextLine();
+        int num = 1;
 
-        System.out.println(gold);
+        System.out.println(num);
 
     }
 }
