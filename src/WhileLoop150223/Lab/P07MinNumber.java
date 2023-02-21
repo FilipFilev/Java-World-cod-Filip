88cc78723b4499384cae40ca188b87562368dab4
filip_filev <aquamarine777@abv.bg>
2023-02-21 16:33:29 +0200

    MInNumber

---
+++ src/WhileLoop150223/Lab/P07MinNumber.java
@@ -0,0 +1,24 @@
+package WhileLoop150223.Lab;
+
+import java.util.Scanner;
+
+public class P07MinNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        int minNum = Integer.MAX_VALUE;
+        while (!input.equals("Stop")) {
+            int currentNum = Integer.parseInt(input);
+
+            if (currentNum < minNum) {
+                minNum = currentNum;
+            }
+
+            input = scanner.nextLine();
+        }
+
+        System.out.println(minNum);
+    }
+}
