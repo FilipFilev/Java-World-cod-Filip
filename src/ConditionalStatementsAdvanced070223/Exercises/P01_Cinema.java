b7f3853341c4b005f22d04ad633a1dc136663dfb
filip_filev <aquamarine777@abv.bg>
2023-02-21 10:52:04 +0200

    Cinema

--- src/ConditionalStatementsAdvanced070223/Exercises/P01_Cinema.java
+++ src/ConditionalStatementsAdvanced070223/Exercises/P01_Cinema.java
@@ -7,6 +7,24 @@ public class P01_Cinema {
         Scanner scanner = new Scanner(System.in);
 
 
+        //•	Premiere – премиерна прожекция, на цена 12.00 лева.
+        //•	Normal – стандартна прожекция, на цена 7.50 лева.
+        //•	Discount – прожекция за деца, ученици и студенти на намалена цена от 5.00 лева.
 
+        String projection = scanner.nextLine();
+        int rows = Integer.parseInt(scanner.nextLine());
+        int columns = Integer.parseInt(scanner.nextLine());
+
+        double income = 0;
+
+        if (projection.equals("Premiere")) {
+            income = rows * columns * 12;
+        } else if (projection.equals("Normal")) {
+            income = rows * columns * 7.50;
+        } else if (projection.equals("Discount")) {
+            income = rows * columns * 5;
+        }
+
+        System.out.printf("%.2f leva", income);
     }
 }
