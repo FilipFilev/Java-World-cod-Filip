e974bfe0b6e69f9aad5a15992d1aaf0d1cbb403a
filip_filev <aquamarine777@abv.bg>
2023-02-21 10:52:46 +0200

    FishingBoat

--- src/ConditionalStatementsAdvanced070223/Exercises/P04_FishingBoat.java
+++ src/ConditionalStatementsAdvanced070223/Exercises/P04_FishingBoat.java
@@ -6,5 +6,41 @@ public class P04_FishingBoat {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int budget = Integer.parseInt(scanner.nextLine());
+        String season = scanner.nextLine();
+        int people = Integer.parseInt(scanner.nextLine());
+
+        double price = 0;
+        switch (season) {
+            case "Spring":
+                price = 3000;
+                break;
+            case "Summer":
+            case "Autumn":
+                price = 4200;
+                break;
+            case "Winter":
+                price = 2600;
+                break;
+        }
+
+        if (people <= 6) {
+            price = price * 0.90;
+        } else if (people <= 11) {
+            price = price * 0.85;
+        } else {
+            price = price * 0.75;
+        }
+
+        if (people % 2 == 0 && !season.equals("Autumn")) {
+            price = price * 0.95;
+        }
+
+        double diff = Math.abs(budget - price);
+        if (budget >= price) {
+            System.out.printf("Yes! You have %.2f leva left.", diff);
+        } else {
+            System.out.printf("Not enough money! You need %.2f leva.", diff);
+        }
     }
 }
