13f56ecfd4119bae291d1daa6ef641adb3d38a9a
filip_filev <aquamarine777@abv.bg>
2023-02-23 09:19:26 +0200

    clock

--- src/NestedLoops220223/Lab/P01_Clock.java
+++ src/NestedLoops220223/Lab/P01_Clock.java
@@ -6,8 +6,12 @@ public class P01_Clock {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        for (int h = 0; h <= 23 ; h++) {
-            System.out.println(h);
+        for (int h = 0; h <= 23; h++) {
+            for (int m = 0; m <= 59; m++) {
+                System.out.printf("%d:%02d%n", h, m);
+
+            }
+
 
         }
     }
