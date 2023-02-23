43775ab143c303552a23be737ce4f23e1fc208c3
filip_filev <aquamarine777@abv.bg>
2023-02-23 13:25:27 +0200

    4

---
+++ src/NestedLoops220223/Lab/P06Building.java
@@ -0,0 +1,12 @@
+package NestedLoops220223.Lab;
+
+
+import java.util.Scanner;
+
+public class P06Building {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
