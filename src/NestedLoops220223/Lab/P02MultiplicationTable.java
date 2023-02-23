bd329102550bfa1c699e85c3bf4aeab1427a133b
filip_filev <aquamarine777@abv.bg>
2023-02-23 13:24:39 +0200

    1

---
+++ src/NestedLoops220223/Lab/P02MultiplicationTable.java
@@ -0,0 +1,19 @@
+package NestedLoops220223.Lab;
+
+
+public class P02MultiplicationTable {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                int result = i * j;
+
+
+                System.out.printf("%d * %d = %d%n", i, j, result);
+            }
+
+        }
+
+
+    }
+}
