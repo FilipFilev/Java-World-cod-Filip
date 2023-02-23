fb754f80f11088adcbbc64e2e1c5d50173ca6ff0
filip_filev <aquamarine777@abv.bg>
2023-02-23 13:24:49 +0200

    2

---
+++ src/NestedLoops220223/Lab/P03Combinations.java
@@ -0,0 +1,10 @@
+package NestedLoops220223.Lab;
+
+import java.util.Scanner;
+
+public class P03Combinations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
