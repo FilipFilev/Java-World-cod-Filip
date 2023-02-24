3101e9eb6ebe5c69e4026f138acc5f3a7a574211
filip_filev <aquamarine777@abv.bg>
2023-02-24 11:44:39 +0200

    if (isValid == true) {                 break;             }         }
            if (!isValid) {
                System.out.printf("%d combinations - neither equals %d%n", count, magicNum);

--- src/NestedLoops220223/Lab/P04SumOfTwoNumbers.java
+++ src/NestedLoops220223/Lab/P04SumOfTwoNumbers.java
@@ -6,5 +6,33 @@ public class P04SumOfTwoNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int firstNum = Integer.parseInt(scanner.nextLine()); // 1
+        int secondNum = Integer.parseInt(scanner.nextLine()); // 10
+        int magicNum = Integer.parseInt(scanner.nextLine());
+
+
+        boolean isValid = false;
+        int count = 0;
+        for (int i = firstNum; i <= secondNum; i++) {
+            for (int j = firstNum; j <= secondNum; j++) {
+                count++;
+                int sum = i + j;
+
+                if (sum == magicNum) {
+                    isValid =true;
+                    System.out.printf("Combination N:%d ", count);
+                    System.out.printf("(%d + %d = %d)%n", i, j, sum);
+                    break;
+                }
+            }
+
+            if (isValid == true) {
+                break;
+            }
+        }
+
+        if (!isValid) {
+            System.out.printf("%d combinations - neither equals %d%n", count, magicNum);
+        }
     }
 }
