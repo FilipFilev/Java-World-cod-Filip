4cd7394d1fac70108957d320338d2b8af8ee8e28
filip_filev <aquamarine777@abv.bg>
2023-02-24 10:51:56 +0200

    P03Combinations

--- src/NestedLoops220223/Lab/P03Combinations.java
+++ src/NestedLoops220223/Lab/P03Combinations.java
@@ -6,5 +6,23 @@ public class P03Combinations {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int n = Integer.parseInt(scanner.nextLine());
+
+        int count = 0;
+        for (int i = 0; i <= n ; i++) {
+            for (int j = 0; j <= n; j++) {
+                for (int k = 0; k <= n ; k++) {
+
+                    int sum = i + j + k;
+                    if (sum == n) {
+                        count++;
+                    }
+
+                }
+
+            }
+
+        }
+        System.out.println(count);
     }
 }
