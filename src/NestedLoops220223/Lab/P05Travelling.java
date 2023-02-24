fda4e78689e5b01320cb7d008b0d301bff605676
filip_filev <aquamarine777@abv.bg>
2023-02-24 15:13:31 +0200

    while (!input.equals("End")) {             String destination = input;             double tripPrice = Double.parseDouble(scanner.nextLine());

--- src/NestedLoops220223/Lab/P05Travelling.java
+++ src/NestedLoops220223/Lab/P05Travelling.java
@@ -6,5 +6,28 @@ public class P05Travelling {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+
+        String input = scanner.nextLine();
+
+        while (!input.equals("End")) {
+            String destination = input;
+            double tripPrice = Double.parseDouble(scanner.nextLine());
+
+
+            double currentSum = 0;
+            while (currentSum < tripPrice) {
+                double amount = Double.parseDouble(scanner.nextLine());
+                currentSum += amount;
+
+            }
+
+
+            System.out.printf("Going to %s!%n", destination);
+
+            input = scanner.nextLine();
+
+
+        }
+
     }
 }
