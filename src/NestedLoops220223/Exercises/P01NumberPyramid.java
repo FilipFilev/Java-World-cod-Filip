e63cbce643eaf42ef2df5633736e9aa9c83a650b
Filip Filev <124343742+FilipFilev@users.noreply.github.com>
2023-02-25 21:33:33 +0200

    demo

---
+++ src/NestedLoops220223/Exercises/P01NumberPyramid.java
@@ -0,0 +1,11 @@
+package NestedLoops220223.Exercises;
+
+import java.util.Scanner;
+
+public class P01NumberPyramid {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
