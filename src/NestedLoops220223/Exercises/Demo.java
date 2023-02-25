e3f87371e9234a114f7205a9ab9450518220215b
Filip Filev <124343742+FilipFilev@users.noreply.github.com>
2023-02-25 21:21:22 +0200

    demo

---
+++ src/NestedLoops220223/Exercises/Demo.java
@@ -0,0 +1,17 @@
+package NestedLoops220223.Exercises;
+
+import java.util.Scanner;
+
+public class Demo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine()); // 123 % 10 = 3
+
+
+
+
+        int lastDigit = number % 10;
+
+        System.out.println(lastDigit);    }
+}
