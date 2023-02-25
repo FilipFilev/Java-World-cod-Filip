0b0fa1d990e805d95480edfb22805ccf3b199c60
Filip Filev <124343742+FilipFilev@users.noreply.github.com>
2023-02-25 21:36:45 +0200

    demo

--- src/NestedLoops220223/Lab/P06Building.java
+++ src/NestedLoops220223/Lab/P06Building.java
@@ -7,6 +7,23 @@ public class P06Building {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int floors = Integer.parseInt(scanner.nextLine()); // 6
+        int rooms = Integer.parseInt(scanner.nextLine()); //4
+
+        for (int f = floors; f >= 1; f--) {
+            for (int r = 0; r < rooms; r++) {
+                if (f == floors) {
+                    System.out.printf("L%d%d ", f, r);
+                } else if (f % 2 == 0) {
+                    System.out.printf("O%d%d ", f, r);
+                } else {
+                    System.out.printf("A%d%d ", f, r);
+                }
+
+            }
+            System.out.println();
+        }
+
 
     }
 }
