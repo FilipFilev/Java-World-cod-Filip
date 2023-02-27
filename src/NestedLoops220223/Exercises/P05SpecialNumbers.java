7c14cf1bec6f072129e879e409a4aa5a1a2106c9
filip_filev <aquamarine777@abv.bg>
2023-02-27 18:05:14 +0200

    demo3

---
+++ src/NestedLoops220223/Exercises/P05SpecialNumbers.java
@@ -0,0 +1,18 @@
+package NestedLoops220223.Exercises;
+
+import java.util.Scanner;
+
+public class P05SpecialNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+
+        for (int currentNum = 1111; currentNum <= 9999; currentNum++) {
+
+
+
+        }
+
+    }
+}
