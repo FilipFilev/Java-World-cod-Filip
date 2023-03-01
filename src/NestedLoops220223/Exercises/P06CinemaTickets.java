6cb47b33d594485ccb920e640ad78e149b3f178d
filip_filev <aquamarine777@abv.bg>
2023-03-01 09:55:08 +0200

    double percentField = countTicketsPerMovie * 1.0 / availableSeats * 100;             System.out.printf("%s - %.2f%% full.%n", currentMovie, percentField);

--- src/NestedLoops220223/Exercises/P06CinemaTickets.java
+++ src/NestedLoops220223/Exercises/P06CinemaTickets.java
@@ -7,31 +7,49 @@ public class P06CinemaTickets {
         Scanner scanner = new Scanner(System.in);
 
         String input = scanner.nextLine();
+        int totalTickets = 0;
+        int countStudentTickets = 0;
+        int countKidTickets = 0;
+        int countStandardTickets = 0;
 
 
-        int countTicketsPerMovie = 0;
         while (!input.equals("Finish")) {
 
             int availableSeats = Integer.parseInt(scanner.nextLine());
             String command = scanner.nextLine();
             String currentMovie = input;
 
+
+            int countTicketsPerMovie = 0;
+
             while (!command.equals("End")) {
                 String ticketType = command;
+                countTicketsPerMovie++;
 
                 switch (ticketType) {
                     case "student":
+                        countStudentTickets++;
                         break;
                     case "standard":
+                        countStandardTickets++;
                         break;
                     case "kid":
+                        countKidTickets++;
                         break;
 
                 }
 
+                if (countTicketsPerMovie >= availableSeats){
+                    break;
+                }
 
                 command = scanner.nextLine();
             }
+
+            totalTickets += countTicketsPerMovie;
+
+            double percentField = countTicketsPerMovie * 1.0 / availableSeats * 100;
+            System.out.printf("%s - %.2f%% full.%n", currentMovie, percentField);
 
 
             input = scanner.nextLine();
