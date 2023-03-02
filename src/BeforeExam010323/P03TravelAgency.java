1f0363899715efec87801d09c1d530ca6fffe4fe
filip_filev <aquamarine777@abv.bg>
2023-03-02 11:09:47 +0200

    P03TravelAgency

---
+++ src/BeforeExam010323/P03TravelAgency.java
@@ -0,0 +1,11 @@
+package BeforeExam010323;
+
+import java.util.Scanner;
+
+public class P03TravelAgency {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
