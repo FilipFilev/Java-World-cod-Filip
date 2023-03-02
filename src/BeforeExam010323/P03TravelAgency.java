109874a7acf0ede4167e3f334f06489e1da5fdcd
filip_filev <aquamarine777@abv.bg>
2023-03-02 14:21:59 +0200

    boolean isValid

--- src/BeforeExam010323/P03TravelAgency.java
+++ src/BeforeExam010323/P03TravelAgency.java
@@ -6,6 +6,57 @@ public class P03TravelAgency {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String town = scanner.nextLine();
+        String typePackage = scanner.nextLine();
+        String VIP = scanner.nextLine();
+        int days = Integer.parseInt(scanner.nextLine());
 
+        boolean isValid = true;
+        double price = 0;
+        if (town.equals("Bansko") || town.equals("Borovets")) {
+            if (typePackage.equals("noEquipment")) {
+                price = 80;
+                if (VIP.equals("yes")) {
+                    price = price * 0.95;
+                }
+            } else if (typePackage.equals("withEquipment")) {
+                price = 100;
+                if (VIP.equals("yes")) {
+                    price = price * 0.90;
+                }
+            } else {
+                isValid = false;
+            }
+
+        } else if (town.equals("Varna") || town.equals("Burgas")) {
+            if (typePackage.equals("noBreakfast")) {
+                price = 100;
+                if (VIP.equals("yes")) {
+                    price = price * 0.93;
+                }
+            } else if (typePackage.equals("withBreakfast")) {
+                price = 130;
+                if (VIP.equals("yes")) {
+                    price = price * 0.88;
+                }
+            } else {
+                isValid = false;
+            }
+
+        } else {
+            isValid = false;
+        }
+
+        if (days <= 0) {
+            System.out.println("Days must be positive number!");
+        } else if (!isValid) {
+            System.out.println("Invalid input!");
+        } else {
+            if (days > 7) {
+                days = days - 1;
+            }
+            double totalPrice = price * days;
+            System.out.printf("The price is %.2flv! Have a nice time!", totalPrice);
+        }
     }
 }
