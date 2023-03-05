cb82f4c8ca49691213b3dbcabe56dcbc2653417f
Filip Filev <124343742+FilipFilev@users.noreply.github.com>
2023-03-05 21:21:28 +0200

    Java

---
+++ src/BeforeExam040323/P5.java
@@ -0,0 +1,9 @@
+package BeforeExam040323;
+
+import java.util.Scanner;
+
+public class P5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
