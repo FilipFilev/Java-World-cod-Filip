88dfdbc46c8fac8d760fc05fa121cf5ed19290c8
Filip Filev <124343742+FilipFilev@users.noreply.github.com>
2023-03-05 21:19:48 +0200

    MovieDay

---
+++ src/BeforeExam040323/P3.java
@@ -0,0 +1,9 @@
+package BeforeExam040323;
+
+import java.util.Scanner;
+
+public class P3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
