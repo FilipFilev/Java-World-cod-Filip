89f4179d3a24f56f34dc3ff3c6a098af07988ad7
filip_filev <aquamarine777@abv.bg>
2023-03-06 15:58:09 +0200

    switch (destination)

--- src/BeforeExam040323/P3EasterTrip.java
+++ src/BeforeExam040323/P3EasterTrip.java
@@ -2,7 +2,7 @@ package BeforeExam040323;
 
 import java.util.Scanner;
 
-public class P3 {
+public class P3EasterTrip {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
