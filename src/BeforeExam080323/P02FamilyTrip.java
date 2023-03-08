1b046e96d596d8b8a425e310d38a99e60e245349
filip_filev <aquamarine777@abv.bg>
2023-03-08 16:13:47 +0200

    p2

---
+++ src/BeforeExam080323/P02FamilyTrip.java
@@ -0,0 +1,31 @@
+package BeforeExam080323;
+
+import java.util.Scanner;
+
+public class P02FamilyTrip {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double budget = Double.parseDouble(scanner.nextLine());
+        int nights = Integer.parseInt(scanner.nextLine());
+        double pricePerNight = Double.parseDouble(scanner.nextLine());
+        int percentAdditional = Integer.parseInt(scanner.nextLine());
+
+        if (nights > 7){
+            pricePerNight = pricePerNight * 0.95;
+        }
+        double sumAllNights = nights * pricePerNight;
+        double expenses = budget * percentAdditional / 100;
+
+        double neededMoney = sumAllNights + expenses;
+
+
+        double diff = Math.abs(budget - neededMoney);
+        if (budget >= neededMoney) {
+            System.out.printf("Ivanovi will be left with %.2f leva after vacation.%n", diff);
+        } else {
+            System.out.printf("%.2f leva needed.", diff);
+        }
+
+    }
+}
