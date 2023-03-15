f941e7bb7062795d4546b59f34a465e122344665
filip_filev <aquamarine777@abv.bg>
2023-03-15 09:42:08 +0200

    new

---
+++ src/AloneExam140323/P2.java
@@ -0,0 +1,11 @@
+package AloneExam140323;
+
+import java.util.Scanner;
+
+public class P2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
