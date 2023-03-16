5257794acdca0fed262fcf07748c1d155321c5ac
filip_filev <aquamarine777@abv.bg>
2023-03-16 18:04:41 +0200

    new

--- src/AloneExam140323/P1.java
+++ src/AloneExam140323/P1.java
@@ -6,6 +6,16 @@ public class P1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int taxPerYear = Integer.parseInt(scanner.nextLine());
+
+        double runners = taxPerYear * 0.60;
+        double suit = runners - (runners * 0.20);
+        double ball = suit * 0.25;
+        double acc = ball * 0.20;
+
+        double totalPrice = taxPerYear + runners + suit + ball + acc;
+
+        System.out.printf("%.2f", totalPrice);
 
     }
 }
