f3bd637f5ac0c36b9afc89f841023e95ea3eeff6
filip_filev <aquamarine777@abv.bg>
2023-03-17 16:51:10 +0200

    modified

--- src/AloneExam160323/P6.java
+++ src/AloneExam160323/P6.java
@@ -1,4 +1,38 @@
 package AloneExam160323;
 
+import java.util.Scanner;
+
 public class P6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        int daysCount = Integer.parseInt(scanner.nextLine());
+        int hoursPerDay = Integer.parseInt(scanner.nextLine());
+
+        double totalSum = 0;
+        for (int day = 1; day <= daysCount ; day++) {
+
+            double price = 0;
+            double currentDaySum = 0;
+
+            for (int hours = 1; hours <= hoursPerDay ; hours++) {
+
+                if (day % 2 == 0 && hours % 2 != 0) {
+                    price = 2.5;
+                } else if (day % 2 != 0 && hours % 2 == 0) {
+                    price = 1.25;
+                } else {
+                    price = 1;
+                }
+
+                currentDaySum += price;
+            }
+
+            totalSum += currentDaySum;
+            System.out.printf("Day: %d - %.2f leva%n", day, currentDaySum);
+        }
+
+        System.out.printf("Total: %.2f leva", totalSum);
+    }
 }
